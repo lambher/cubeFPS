8cdda18387b8a153ed63b382bacf90c2cc37b271
Lambert Teisserenc <lambert.teisserenc@gmail.com>
2021-04-21 14:32:10 +0200

    update conf

--- conf/conf.go
+++ conf/conf.go
@@ -3,6 +3,6 @@ package conf
 import "time"
 
 const TickTimeClient = time.Millisecond * 50
-const TickTimeServer = time.Millisecond * 500
+const TickTimeServer = time.Millisecond * 50
 const Port = 8888
 const Host = "5.39.93.173"
