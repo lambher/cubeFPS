4ff07d4f6e1d10d7d329ec0c4675c456397e6b87
Lambert Teisserenc <lambert.teisserenc@gmail.com>
2021-04-21 15:13:21 +0200

    update moves

--- models/player.go
+++ models/player.go
@@ -234,8 +234,8 @@ func (p *Player) Refresh(player Player) {
 	p.Position = player.Position
 	p.Direction = player.Direction
 	p.Velocity = player.Velocity
-	//p.HorizontalAngle = player.HorizontalAngle
-	//p.VerticalAngle = player.VerticalAngle
+	p.HorizontalAngle = player.HorizontalAngle
+	p.VerticalAngle = player.VerticalAngle
 }
 
 func (p *Player) RefreshMoves(moves *Moves) {
